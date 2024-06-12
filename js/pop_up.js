c969e87ce086b54e5c8ab4b2255c60f999dc5e03
ThomasYang <silent5958@gmail.com>
2024-06-12 17:54:20 +0900

    final

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,5 +1,5 @@
 var close_time; //시간정보
-var close_time2 = 50; //10초 설정
+var close_time2 = 10; //10초 설정
 
 
 function addJavascript(jsname) { // 자바스크립트 외부 연동
@@ -13,6 +13,8 @@ function addJavascript(jsname) { // 자바스크립트 외부 연동
 addJavascript('/js/security.js'); // 암복호화 함수
 addJavascript('/js/session.js'); // 세션 함수
 addJavascript('/js/cookie.js'); // 쿠키 함수
+//addJavascript('/js/destroy.js');
+
 
 
 function pop_up(){
@@ -23,51 +25,18 @@ function pop_up(){
 } //가로400, 세로300, 위 10, 왼쪽 10 여백
 //window.open()함수는 창을 열거나 닫는(.close) 전용 함수
 
-function show_time(){
-    let divClock = document.getElementById("Time");
-    divClock.innerText = close_time2; //10초 삽입 시작
-    close_time2--; //1초씩 감소
-    setTimeout(show_time, 1000); //1초마다 갱신
+function show_time() {
+    let divClock = document.getElementById('Time');
+    if (divClock) {
+        divClock.innerText = close_time2;
+        close_time2--;
+        setTimeout(show_time, 1000);
+    } 
 }
 
 function out(obj) {
-    obj.src="image/LOGO.png";
+    obj.src="image/LOGO.PNG";
 }
-
-/*function close_window(){
-    window.close();
-}*/
-
-
-
-/*function setCookie(name, value, expiredays) {
-    var date = new Date();
-    date.setDate(date.getDate() + expiredays);
-    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
-
-}
-    
-
-
-
-function getCookie(name){
-    var cookie = document.cookie;
-    //console.log("쿠키를 요청합니다.");
-    //console.log(cookie);
-    if(cookie != ""){
-        var cookie_array = cookie.split("; ");
-        //console.log(cookie_array);
-        for(var index in cookie_array){
-            var cookie_name = cookie_array[index].split("=");
-            console.log(cookie_name);
-            if(cookie_name[0] == "popupYN"){
-                return cookie_name[1];
-            }
-        }
-        
-    }
-    return ;
-}*/
 
 function closePopup(){
     if(document.getElementById(`check_popup`).value){
@@ -79,7 +48,7 @@ function closePopup(){
 
 
 clearTimeout(close_time); //재호출정지
-close_time = setTimeout("close_window()", 50000); //1/1000 초 지정, 바로시작
+close_time = setTimeout("close_window()", 10000); //1/1000 초 지정, 바로시작
 show_time();//
 //================================================================================================//
 function show_clock(){
